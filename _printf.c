f04c727283351d8963fc1c136cf8c596b2b17054
tao08 <1823@holbertonschool.com>
2020-07-20 14:04:51 -0500

    estructura inicial

--- _printf.c
+++ _printf.c
@@ -1,31 +1,49 @@
-#include"holberton.h"
+#include "holberton.h"
+#include <stdarg.h>
+#include <stdio.h>
 /**
- * prin_char - print char.
- * @valist: char to print.
- */
-void prin_char(va_list valist)
+* _printf - main function to print in console
+* @format: array to print and check type
+* Return: count of character printed
+**/
+int _printf(const char *format, ...)
 {
-	printf("%c", va_arg(valist, int));
+	int count = -1;
+
+	if (format != NULL)
+	{
+		int g;
+		va_list ar_list;
+		int (*o)(va_list);
+
+		va_start(ar_list, format);
+
+		if (format[0] == '%' && format[1] == '\0')
+			return (-1);
+
+		count = 0;
+
+		for (g = 0; format[g] != '\0'; g++)
+		{
+			if (format[g] == '%')
+			{
+				if (format[g + 1] == '%')
+				{
+					count += _putchar(format[g]);
+					g++;
+				}
+				else if (format[g + 1] != '\0')
+				{
+					o = get_func(format[g + 1]);
+					count += (o ? o(ar_list) : _putchar(format[g]) + _putchar(format[i + 1]));
+					g++;
+				}
+			}
+			else
+				count += _putchar(format[g]);
+		}
+		va_end(ar_list);
+	}
+
+	return (count);
 }
-/**
- * prin_integer - print integer.
- * @valist: int to print.
- */
-void prin_integer(va_list valist)
-{
-	printf("%d", va_arg(valist, int));
-}
-/**
- * prin_float - print float.
- * @valist: float to print.
- */
-void prin_float(va_list valist)
-{
-	printf("%f", va_arg(valist, double));
-}
-/**
- * prin_string - print string.
- * @valist: string to print.
- */
-void prin_string(va_list valist)
-{
