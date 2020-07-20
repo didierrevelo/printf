3f8c9b4085eb3a141d3909532123069ec2f00d30
tao08 <1823@holbertonschool.com>
2020-07-20 13:15:23 -0500

    estructura

---
+++ _printf.c
@@ -0,0 +1,31 @@
+#include"holberton.h"
+/**
+ * prin_char - print char.
+ * @valist: char to print.
+ */
+void prin_char(va_list valist)
+{
+	printf("%c", va_arg(valist, int));
+}
+/**
+ * prin_integer - print integer.
+ * @valist: int to print.
+ */
+void prin_integer(va_list valist)
+{
+	printf("%d", va_arg(valist, int));
+}
+/**
+ * prin_float - print float.
+ * @valist: float to print.
+ */
+void prin_float(va_list valist)
+{
+	printf("%f", va_arg(valist, double));
+}
+/**
+ * prin_string - print string.
+ * @valist: string to print.
+ */
+void prin_string(va_list valist)
+{
