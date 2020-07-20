500bc27cff5781e72d2e82990a1b5cdfa1c82ff9
didierrevelo <1829@holbertonschool.com>
2020-07-20 13:39:45 -0500

    2

--- holberton.h
+++ holberton.h
@@ -1,7 +1,26 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct all - this structure contine the the symbol of function.
+ * 
+ * @enter: operator function
+ * @far: function linked
+ */
+typedef struct all {
+    char *enter;
+    int (*far)(va_list);
+}all_t;
+
+
+int _printf(const char *format, ...);
+int print_c(va_list);
+int print_s(va_list);
+int print_p(va_list);
 
 
 
-#endif /* HOLBERTON_H */
+#endif
