8f74b7d72d916dcddde1cf6acfa272fe6799ba21
didierrevelo <1829@holbertonschool.com>
2020-07-21 17:31:28 -0500

    12

--- formats_f.c
+++ formats_f.c
@@ -51,3 +51,38 @@ int print_i(va_list ls)
     numbers = p_number(ls);
     return (numbers);
 }
+/**
+ * p_number - print numbers
+ * @lisa: arguments
+ * Return: print numbers
+ */
+int p_number(va_list lisa)
+{
+    int a, b, c;
+    unsigned int bers;
+
+    a = va_arg(lisa, int);
+    b = 1;
+    c = 0;
+
+    if (a < 0)
+    {
+        c += _put_char('-');
+        bers = a * -1;
+    }
+    else
+    {
+        bers = a;
+    }
+
+    while (bers / b > 9)
+    b *= 10;
+
+    while (b != 0)
+    {
+        c +=_put_char('0' + bers / b);
+        bers %= b;
+        b /= 10;
+    }
+    return (bers);
+}
