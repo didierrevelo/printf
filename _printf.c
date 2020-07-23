cfa3a5b6319503ce6a45beb031b8b4f373108aa9
tao08 <1823@holbertonschool.com>
2020-07-23 10:27:20 -0500

    estructura

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * _printf - print anything
- * @format: argument
+ * @format: argument of function
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
